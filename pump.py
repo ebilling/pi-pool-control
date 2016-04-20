4e71eeddb8473f059aba6a4fd56edd37a027ced1
Eric Billingsley <ebilling@babrains.com>
2016-04-20 16:10:40 -0700

    Restore from latest backup.

--- pump.py
+++ pump.py
@@ -1,23 +1,35 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
+import time
 
 PUMP_GPIO = (17, 27)
 SWEEP_GPIO = (22, 23)
+
+START_TIME = None
 
 # Initialize GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
 def turnOn(gpios):
+    global START_TIME
     print "Turning on %s" % (str(gpios))
     for gpio in gpios:
         GPIO.output(gpio, True)
-    
+    START_TIME = time.time()
+
 
 def turnOff(gpios):
+    global START_TIME
     print "Turning off %s" % (str(gpios))
     for gpio in gpios:
         GPIO.output(gpio, False)
+    START_TIME = None
+
+
+def getStartTime():
+    global START_TIME    
+    return START_TIME
 
 
 def getStatus(gpios=PUMP_GPIO + SWEEP_GPIO):
